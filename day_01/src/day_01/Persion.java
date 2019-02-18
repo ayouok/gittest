4c543013a473e07b09b17409b38fab22003ff099
gzw <2540845958@qq.com>
2019-02-18 21:37:09 +0800

    修改了Persion.java Signed-off-by: gzw <2540845958@qq.com>

--- day_01/src/day_01/Persion.java
+++ day_01/src/day_01/Persion.java
@@ -2,7 +2,9 @@ package day_01;
 
 public class Persion {
 	public String GetInfo(){
-	System.out.print("888");	return "666";
+	System.out.print("888");
+	System.out.print("777");
+	return "666";
 	}
 
 }
